91e3ab4c64778b6a46a6a82f6097456776974cb1
helmi <helmitira@yahoo.com>
2019-04-08 12:11:10 -0400

    Updates

---
+++ precache-manifest.a16c235d5e52a2ee6d78154babda0f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "17b086c6266624523196",
+    "url": "/Clicky-Game/static/js/main.21749259.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "17b086c6266624523196",
+    "url": "/Clicky-Game/static/css/main.d7e39191.chunk.css"
+  },
+  {
+    "revision": "4ec9a9a50b384984e037f74e7d930a88",
+    "url": "/Clicky-Game/index.html"
+  }
+];
